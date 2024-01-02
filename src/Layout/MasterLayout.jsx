869d91a0c0a0d697fe19e44c7fe8cf5aa903d28e
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-03 02:12:35 +0800

    moved main navigation from components to layout sections

--- src/Layout/MasterLayout.jsx
+++ src/Layout/MasterLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import MainNavigation from "../components/MainNavigation";
+import MainNavigation from "./sections/MainNavigation";
 
 function MasterLayout() {
   return (
