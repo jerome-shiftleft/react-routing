685bdaf1b241157f806a90154ef65756028a144f
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-03 03:33:32 +0800

    fixed relative paths on products list

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,9 +6,9 @@ function ProductsPage() {
       <main>
         <h1>Products</h1>
         <ul>
-          <li><Link to="/products/1">Product 1</Link></li>
-          <li><Link to="/products/2">Product 2</Link></li>
-          <li><Link to="/products/3">Product 3</Link></li>
+          <li><Link to="1">Product 1</Link></li>
+          <li><Link to="2">Product 2</Link></li>
+          <li><Link to="3">Product 3</Link></li>
         </ul>
       </main>
     </>
