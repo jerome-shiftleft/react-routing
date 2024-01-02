2a8beecd818a9a9b370b7c4b78720e2d3b087a80
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-03 02:53:14 +0800

    replaced Link with NavLink

--- src/Layout/sections/MainNavigation.jsx
+++ src/Layout/sections/MainNavigation.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Stack from '@mui/material/Stack';
 
 function MainNavigation() {
   return (
     <>
       <header>
-        <nav>
-          <Stack direction="row" spacing={3}>
-            <Link to="/">Home</Link>
-            <Link to="/About">About</Link>
+        <nav id="main-nav">
+          <Stack className="nav-links" direction="row" spacing={3}>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/About">About</NavLink>
           </Stack>
         </nav>
       </header>
